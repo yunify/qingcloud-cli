3aba84ae61e94de4f66adb5ed59d5e9726dcf90b
Simon <simon@yunify.com>
2013-09-07 12:26:51 +0800

    Version 0.5.5

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def prepare_config_file():
 
 setup(
     name = 'qingcloud-cli',
-    version = '0.5.4',
+    version = '0.5.5',
     description = 'Command Line Interface for QingCloud.',
     long_description = open('README.rst', 'rb').read().decode('utf-8'),
     keywords = 'qingcloud iaas cli',

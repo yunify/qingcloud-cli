3df4c847ee640a4d96b9e0cf5f4a7db2942e2db9
Simon <simon@yunify.com>
2013-09-07 10:41:59 +0800

    Add completer

--- qingcloud_cli/driver.py
+++ qingcloud_cli/driver.py
@@ -2,30 +2,27 @@
 
 import argparse
 import sys
-#from difflib import get_close_matches
 
 from qingcloud_cli.iaas_client.actions import ActionManager
 
 SERVICES = ('iaas')
-
-parser = argparse.ArgumentParser(
-    prog = 'qingcloud',
-    usage = '''%(prog)s iaas <action> [parameters]
-
-    Here are valid actions:
-    ## instance
-    run-instances, start-instances, stop-instances
-    ''',
-)
+valid_actions = ActionManager.action_table().keys()
+valid_actions.sort()
 
 def exit_due_to_invalid_args():
+    usage = '%(prog)s iaas <action> [parameters]\n\nHere are valid actions:\n\n'
+    usage += '\n'.join(valid_actions)
+    parser = argparse.ArgumentParser(
+        prog = 'qingcloud',
+        usage = usage,
+    )
     parser.print_help()
     sys.exit(-1)
 
 def check_argument(args):
     if len(args) < 3:
         exit_due_to_invalid_args()
-    if args[1] not in SERVICES or args[2] not in ActionManager.action_table():
+    if args[1] not in SERVICES or args[2] not in valid_actions:
         exit_due_to_invalid_args()
 
 def get_action(service, action):

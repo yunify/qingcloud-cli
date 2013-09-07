4a0abc12203113819119a6d86d88a94998f6a6c9
Simon <simon@yunify.com>
2013-09-07 10:54:17 +0800

    Fix typo

--- qingcloud_cli/iaas_client/actions/image/delete_images.py
+++ qingcloud_cli/iaas_client/actions/image/delete_images.py
@@ -7,11 +7,11 @@ class DeleteImagesAction(BaseAction):
 
     action = 'DeleteImages'
     command = 'delete-images'
-    MSG_USAGE = '%(prog)s -i image_id, ... [-f <conf_file>]'
+    usage = '%(prog)s -i image_id, ... [-f <conf_file>]'
 
     @classmethod
     def add_ext_arguments(cls, parser):
-        
+
         parser.add_argument('-i', '--images', dest='images',
                 action='store', type=str, default='',
                 help='The comma separated IDs of images you want to delete. ')
@@ -21,5 +21,5 @@ class DeleteImagesAction(BaseAction):
         images = explode_array(options.images)
         if not images:
             return None
-            
+
         return {'images': images}

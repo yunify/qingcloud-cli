55af2016102ec16a4ec3878f45306e3ac4d520e6
Simon <simon@yunify.com>
2013-11-13 18:24:39 +0800

    Add login mode to reset-instances

--- qingcloud/cli/iaas_client/actions/instance/reset_instances.py
+++ qingcloud/cli/iaas_client/actions/instance/reset_instances.py
@@ -8,7 +8,7 @@ class ResetInstancesAction(BaseAction):
 
     action = 'ResetInstances'
     command = 'reset-instances'
-    usage = '%(prog)s -i "instance_id, ..." [-f <conf_file>]'
+    usage = '%(prog)s -i "instance_id, ..." [-f <conf_file> -m <login_mode> -p <login_passwd> -k <login_keypair>]'
 
     @classmethod
     def add_ext_arguments(cls, parser):
@@ -16,6 +16,18 @@ class ResetInstancesAction(BaseAction):
         parser.add_argument('-i', '--instances', dest='instances',
                 action='store', type=str, default='',
                 help='the comma separated IDs of instances you want to reset.')
+
+        parser.add_argument('-l', '--login_mode', dest='login_mode',
+                action='store', type=str, default=None,
+                help='SSH login mode: keypair or passwd')
+
+        parser.add_argument('-p', '--login_passwd', dest='login_passwd',
+                action='store', type=str, default=None,
+                help='login_passwd, should specified when SSH login mode is "passwd".')
+
+        parser.add_argument('-k', '--login_keypair', dest='login_keypair',
+                action='store', type=str, default=None,
+                help='login_keypair, should specified when SSH login mode is "keypair".')
 
         return parser
 
@@ -26,4 +38,9 @@ class ResetInstancesAction(BaseAction):
             print 'error: [instances] should be specified'
             return None
 
-        return {'instances': instances}
+        return {
+                'instances': instances,
+                'login_mode': options.login_mode,
+                'login_passwd': options.login_passwd,
+                'login_keypair': options.login_keypair,
+                }

cdd4a34559a7f1281399288d943f1158857f2829
Simon <simon@yunify.com>
2016-01-13 01:38:05 +0800

    Add vpc_network to create_routers

--- qingcloud/cli/iaas_client/actions/router/create_routers.py
+++ qingcloud/cli/iaas_client/actions/router/create_routers.py
@@ -27,27 +27,32 @@ class CreateRoutersAction(BaseAction):
         parser.add_argument('-c', '--count', dest='count',
                 action='store', type=int, default=1,
                 help='the number of routers to create.')
-        
+
         parser.add_argument('-N', '--router_name', dest='router_name',
                 action='store', type=str, default='',
                 help='the short name of routers')
-        
+
         parser.add_argument('-s', '--security_group', dest='security_group',
                 action='store', type=str, default='',
                 help='ID of the security group you want to apply to router, use default security group if not specified')
-        
+
+        parser.add_argument('-n', '--vpc_network', dest='vpc_network',
+                action='store', type=str, default=None,
+                help='VPC IP addresses range, currently support "192.168.0.0/16" or "172.16.0.0/16", required in zone pek3a')
+
     @classmethod
     def build_directive(cls, options):
         required_params = {
                 'router_name': options.router_name,
-                } 
+                }
         for param in required_params:
             if required_params[param] is None or required_params[param] == '':
                 print('error: [%s] should be specified' % param)
                 return None
 
         return {
-                'count' : options.count, 
+                'count' : options.count,
                 'router_name' : options.router_name,
                 'security_group': options.security_group,
+                'vpc_network': options.vpc_network,
                 }

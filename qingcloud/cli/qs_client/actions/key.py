af7fcd5cdae998b3971b5178233ccae598087aae
Osier Yang <osier@yunify.com>
2016-08-16 18:07:22 +0800

    qingstor: Change the default Content-Type for object to application/octet-stream
    Since "application/oct-stream" is just typo, see
    http://www.iana.org/assignments/media-types/media-types.xhtml for details.

--- qingcloud/cli/qs_client/actions/key.py
+++ qingcloud/cli/qs_client/actions/key.py
@@ -63,7 +63,7 @@ class CreateObjectAction(BaseAction):
             "-t",
             "--type",
             dest="type",
-            default="application/oct-stream",
+            default="application/octet-stream",
             help="The object type"
         )
         return parser

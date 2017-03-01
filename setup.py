ab95e42dfee84baf0f11ab73244b59a398237db0
Simon <simon@yunify.com>
2017-03-01 17:21:18 +0800

    Update version to 1.1.4

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ else:
 
 setup(
     name = 'qingcloud-cli',
-    version = '1.1.3',
+    version = '1.1.4',
     description = 'Command Line Interface for QingCloud.',
     long_description = open('README.rst', 'rb').read().decode('utf-8'),
     keywords = 'qingcloud iaas qingstor cli',

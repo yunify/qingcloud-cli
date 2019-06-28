3eec07540b5fa1f013ba3b994a39eff94ad5f3a1
zhangchi <zc@zhangchideMacBook-Pro.local>
2019-06-28 15:23:03 +0800

    support_custom_protocol_and_port

--- qingcloud/cli/qs_client/actions/base.py
+++ qingcloud/cli/qs_client/actions/base.py
@@ -20,7 +20,7 @@ from argparse import ArgumentParser
 from qingcloud.qingstor.connection import QSConnection
 
 from ...misc.utils import load_conf
-from ..constants import DEFAULT_ENDPOINT
+from ..constants import DEFAULT_ENDPOINT, DEFAULT_PROTOCOL, DEFAULT_PORT
 
 class BaseAction(object):
 
@@ -64,13 +64,20 @@ class BaseAction(object):
             host = endpoint
         else:
             host = "%s.%s" % (conf["zone"], endpoint)
-        extra_options = {}
-        if 'protocol' in conf:
-            extra_options['protocol'] = conf['protocol']
-        if 'port' in conf:
-            extra_options['port'] = conf['port']
+
+        protocol = conf.get('protocol', DEFAULT_PROTOCOL)
+        if protocol not in ['http', 'https']:
+            print("Unsupported protocol [%s] " % protocol)
+            sys.exit(-1)
+
+        port = conf.get('port', DEFAULT_PORT)
+        if not isinstance(port, int):
+            print("The port parameter [%s] should be int" % port)
+            sys.exit(-1)
+
         return QSConnection(qy_access_key_id=conf["qy_access_key_id"], \
-            qy_secret_access_key=conf["qy_secret_access_key"], host=host, **extra_options)
+            qy_secret_access_key=conf["qy_secret_access_key"], host=host, \
+            protocol=protocol, port=port)
 
     @classmethod
     def send_request(cls, options):

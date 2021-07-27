d3107a8d40d17760e5f6a4180d8477b91c269b71
knktc <me@knktc.com>
2021-07-27 09:40:48 +0800

    fix a python3 compatibility issue, print should be used as a function

--- qingcloud/cli/iaas_client/actions/sdwan/get_wan_monitor.py
+++ qingcloud/cli/iaas_client/actions/sdwan/get_wan_monitor.py
@@ -77,7 +77,7 @@ class GetWanMonitorAction(BaseAction):
 
         for param in required_params:
             if required_params[param] is None or required_params[param] == '':
-                print "param [%s] should be specified" % param
+                print("param [%s] should be specified" % param)
                 return None
 
         directive = {"resource": options.resource,
